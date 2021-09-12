d041212eec1c9ae38d6fba5398b983830773f755
Natcha Luangaroonchai <me@nomkhonwaan.com>
2021-09-12 20:45:37 +0700

    Set default widget to RecentPosts

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/widgets.dart';
+import 'package:myblog_ui/recent_posts.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      
+      child: RecentPosts(),
     );
   }
 }
